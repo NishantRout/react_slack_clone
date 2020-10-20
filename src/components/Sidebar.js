2ff7d09211f2c5c5e62ffb5176ff84fbe3085c15
NishantRout <nishantnarayanrout@gmail.com>
2020-10-20 16:31:36 +0530

    Sidebar component UI

---
+++ src/components/Sidebar.js
@@ -0,0 +1,49 @@
+import {
+  Add,
+  Apps,
+  BookmarkBorder,
+  Create,
+  Drafts,
+  ExpandLess,
+  ExpandMore,
+  FiberManualRecord,
+  FileCopy,
+  Inbox,
+  InsertComment,
+  PeopleAlt,
+} from "@material-ui/icons";
+import React from "react";
+import "./css/Sidebar.css";
+import SidebarOption from "./SidebarOption";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <div className="sidebar__info">
+          <h2>Perfect Shades</h2>
+          <h3>
+            <FiberManualRecord />
+            Nishant Rout
+          </h3>
+        </div>
+        <Create />
+      </div>
+      <SidebarOption Icon={InsertComment} title="Threads" />
+      <SidebarOption Icon={Inbox} title="Mentions & Reactions" />
+      <SidebarOption Icon={Drafts} title="Saved Items" />
+      <SidebarOption Icon={BookmarkBorder} title="Channel Browser" />
+      <SidebarOption Icon={PeopleAlt} title="People & User Groups" />
+      <SidebarOption Icon={Apps} title="Apps" />
+      <SidebarOption Icon={FileCopy} title="File Browser" />
+      <SidebarOption Icon={ExpandLess} title="Show Less" />
+      <hr />
+      <SidebarOption Icon={ExpandMore} title="Channels" />
+      <hr />
+      <SidebarOption Icon={Add} title="Add Channel" />
+      <SidebarOption title="YouTube" />
+    </div>
+  );
+}
+
+export default Sidebar;
